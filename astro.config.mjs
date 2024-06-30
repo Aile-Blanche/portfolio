29138782e91549520cc9c3f8c3bde67814b056c3
Aile-Blanche <alexis.boniface3@gmail.com>
2024-07-01 01:32:52 +0200

    opé-2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aile-blanche.github.io',
-    base: '/portfolio/',
-    redirects: {
-        '/professionnal_projects/[...slug]': '/portfolio/professionnal_projects/[...slug]',
-
-    }
+    base: '/portfolio/'
 });
